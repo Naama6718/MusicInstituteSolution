9dff07f8d90924fa23756a76008f3d77bc02e33e
Naama6718 <naama6718@gmail.com>
2025-04-29 14:09:05 +0300

    We add classes to DAL

---
+++ MusicInstitute.DAL/Api/IStudents_Manager_DAL.cs
@@ -0,0 +1,13 @@
+﻿using MusicInstitute.DAL.Models;
+
+namespace MusicInstitute.DAL.Api
+{
+    internal interface IStudents_Manager_DAL
+    {
+        Task AddStudent(Student student);
+        Task DeleteStudent(int studentId);
+        Task<List<Student>> GetAllStudent();
+        Task<Student> GetStudentById(int studentId);
+        Task UpdateStudent(int studentId, string currentPassword, string firstName = null, string lastName = null, string phone = null, string email = null, string instrument = null, int level = 0, string studentPassword = null);
+    }
+}
---
+++ MusicInstitute.DAL/Services/Instrument_Manager_DAL.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicInstitute.DAL.Api;
+using MusicInstitute.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicInstitute.DAL.Services
+{
+    class Instrument_Manager_DAL : IInstrument_Manager_DAL
+    {
+        private readonly DB_Manager _dbManager;
+        public Instrument_Manager_DAL(DB_Manager dbManager)
+        {
+            _dbManager = dbManager;
+        }
+        public async Task<List<Instrument>> GetAllInstruments()
+        {
+            List<Instrument> instruments = await _dbManager.Instruments.ToListAsync();
+            if (instruments == null)
+            {
+                throw new InvalidOperationException("Instruments collection is not initialized.");
+            }
+            return instruments;
+        }
+        public async Task AddInstrument(Instrument instrument)
+        {
+            await _dbManager.Instruments.AddAsync(instrument);
+            await _dbManager.SaveChangesAsync();
+        }
+        public async Task DeleteInstrument(int instrumentId)
+        {
+            var existingInstrument = await _dbManager.Instruments.FirstOrDefaultAsync(i => i.InstrumentId == instrumentId);
+            if (existingInstrument == null)
+                throw new Exception("Student not found");
+
+            _dbManager.Instruments.Remove(existingInstrument);
+            await _dbManager.SaveChangesAsync();
+        }
+        public async Task AddTeacherInstrument(int instrumentId, Teacher teacher)
+        {
+            var instrument = await _dbManager.Instruments.FirstOrDefaultAsync(i => i.InstrumentId == instrumentId);
+            if (instrument == null)
+                throw new Exception("Instrument not found");
+            instrument.Teachers.Add(teacher);
+            await _dbManager.SaveChangesAsync();
+        }
+
+    }
+}
