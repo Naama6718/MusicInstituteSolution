30761b0851e75ec0c5524534cdc156bfed521217
naamasaadon <Hand3D24@mbjcomp.org.il>
2025-05-06 16:19:36 +0300

    Start BL...

---
+++ MusicInstitute.BL/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MusicInstitute.BL.Models;
+
+public partial class Student
+{
+    public int StudentId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Instrument { get; set; } = null!;
+
+    public int Level { get; set; }
+
+    public string StudentPassword { get; set; } = null!;
+
+    public virtual ICollection<BookedLesson> BookedLessons { get; set; } = new List<BookedLesson>();
+
+    public virtual ICollection<PassedLesson> PassedLessons { get; set; } = new List<PassedLesson>();
+}
